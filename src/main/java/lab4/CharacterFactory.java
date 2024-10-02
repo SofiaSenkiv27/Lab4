6713d10659ec1832778b7383853bbecd535e2c01
SofiaSenkiv27 <senkiv.pn@ucu.udu.ua>
2024-10-02 20:14:37 +0300

    added CharacterFactory and GameManager

---
+++ src/main/java/lab4/CharacterFactory.java
@@ -0,0 +1,15 @@
+package lab4;
+import java.util.Random;
+class CharacterFactory {
+    public Character createCharacter() {
+        Random rand = new Random();
+        int characterType = rand.nextInt(5);
+        switch (characterType) {
+            case 0: return new Hobbit();
+            case 1: return new Elf();
+            case 2: return new King();
+            case 3: return new Knight();
+            default: return new Hobbit();
+        }
+    }
+}
---
+++ src/main/java/lab4/GameManager.java
@@ -0,0 +1,33 @@
+package lab4;
+
+class GameManager {
+    public void fight(Character c1, Character c2) {
+        System.out.println("Fight starts between " + c1.getClass().getSimpleName() + " and " + c2.getClass().getSimpleName() + "!");
+        
+
+        while (c1.isAlive() && c2.isAlive()) {
+            c1.kick(c2);
+            if (!c2.isAlive()) {
+                System.out.println(c2.getClass().getSimpleName() + " is defeated!");
+                break;
+            }
+
+            c2.kick(c1);
+            if (!c1.isAlive()) {
+                System.out.println(c1.getClass().getSimpleName() + " is defeated!");
+            }
+        }
+
+        System.out.println("Fight is over.");
+    }
+
+    public static void main(String[] args) {
+        CharacterFactory factory = new CharacterFactory();
+        GameManager gameManager = new GameManager();
+
+        Character hero1 = factory.createCharacter();
+        Character hero2 = factory.createCharacter();
+
+        gameManager.fight(hero1, hero2);
+    }
+}
